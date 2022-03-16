0f25879052dfe6ae55d8035256c2c08270bf018f
Ogweno <84031459+Ogweno-D@users.noreply.github.com>
2022-03-16 09:09:24 +0300

    Update F1.NANRV.c

--- F1.NANRV.c
+++ F1.NANRV.c
@@ -1,21 +1,27 @@
 #include<stdio.h>
 
-void area();//The function does not have any arguments.
+void area();  //The function does not have any arguments.
 
 void main()
 {
+	//The function call.
 	area();
 }
 
+
+//Function definition.
 void  area()
 {
     float area;
     float r;
+	
+    //Prompt the user to enter the radius of the circle.
     printf("enter the radius of the circle:");
     scanf("%f",&r);
     
     area=3.142 *r*r;
-    
+	
+    //Display the area of the circle.
     printf("The area of the circle is %f",area);
 
     //No return value.
